5e3593d5ce45084b87a231b679c1af04b047c376
adjit <ajtawil@gmail.com>
2016-10-07 02:30:45 -0400

    Added class structures for envision partners and the price list
    Add the class structure for the enVision partner List - created
    enVisionPartnerList class and all helper classes in order to store the
    data from the enVIsion partner list. Also created the epsonPriceLIst
    class and most helper functions in order to store the data from the
    price list.
    
    Next step would be to finish all initializer functions and then figure
    out how to run them all asymptotically to increase speed.

---
+++ EpsonPOSReport/enVisionPartnerListClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsonPOSReport
+{
+    public enum enVisionLevels
+    {
+        COLORWORKS_OEM,
+        COLORWORKS_PLUS,
+        COLORWORKS_PREMIER,
+        DISCPRODUCER,
+        M_SELECT_ISV,
+        PLUS_OEM,
+        PLUS_VAR,
+        PREMIER_OEM,
+        PREMIER_VAR,
+        SELECT_VAR,
+        SOFTWARE_SOLUTION,
+        TECH
+    }
+
+    public enum priceLevels
+    {
+        Select = 2,
+        Plus = 3,
+        Premier = 3,
+        eFi = 4,
+        mSelect
+    }
+
+    class enVisionPartnerList
+    {
+        public List<enVisionCustomer> customers { get; }
+
+        public void addCustomer(int priceLevelIndex, string enVisionLevel, string customer,
+                                string enVisionNumber, string priceLevel)
+        {
+            customers.Add(new enVisionCustomer(priceLevelIndex, enVisionLevel, customer,
+                                                enVisionNumber, priceLevel));
+        }
+
+        public enVisionCustomer getCustomer(string identifier, bool isEnVisionNumber)
+        {
+            enVisionCustomer customer = null;
+
+            if(isEnVisionNumber)
+            {
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    if (customers[i].enVisionNumber == identifier) return customers[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < customers.Count; i++)
+                {
+                    if (customers[i].customer == identifier) return customers[i];
+                }
+            }
+
+            return customer;
+        }
+    }
+
+    class enVisionCustomer
+    {
+        public int priceLevelIndex { get; }
+        public string enVisionLevel { get; }
+        public string customer { get; }
+        public string enVisionNumber { get; }
+        public string priceLevel { get; }
+
+        public enVisionCustomer(int priceLevelIndex, string enVisionLevel, string customer,
+                                string enVisionNumber, string priceLevel)
+        {
+            this.priceLevelIndex = priceLevelIndex;
+            this.enVisionLevel = enVisionLevel;
+            this.customer = customer;
+            this.enVisionNumber = enVisionNumber;
+            this.priceLevel = priceLevel;
+        }
+    }
+}
---
+++ EpsonPOSReport/epsonPriceListClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsonPOSReport
+{
+    class epsonPriceList
+    {
+        public List<item> items { get; }
+
+        public void addItem(item newItem)
+        {
+            items.Add(newItem);
+        }
+
+        public item getItem(string cCode)
+        {
+            for(int i = 0; i < items.Count; i++)
+            {
+                if (items[i].cCode == cCode) return items[i];
+            }
+            return null;
+        }
+    }
+
+    public class item
+    {
+        public string cCode { get; set; }
+        public double Cost { get; set; }
+        public priceLevel Select { get; set; }
+        public priceLevel Plus { get; set; }
+        public priceLevel Premier { get; set; }
+        public priceLevel mSelect { get; set; }
+        public priceLevel eFi { get; set; }
+        public priceLevel Colorworks { get; set; }
+    }
+
+    public class priceLevel
+    {
+        public double rebate { get; set; }
+        public double fulfillment { get; set; }
+    }
+}
