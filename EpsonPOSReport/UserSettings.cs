cc41f1a86e06216eb63e6038ebfdcd07153868c3
adjit <ajtawil@gmail.com>
2016-10-14 08:49:38 -0400

    Added User Settings Class
    Added user settings class to handle all of the constants like file
    locations etc.
    
    Need to explore how to handle the program codes, and how to check if
    there has been an extra program code added. Possibly make another
    setting that would hold extra program program codes.
    
    Will need to find a way in order to know when and where to use that
    program code.

---
+++ EpsonPOSReport/UserSettings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpsonPOSReport
+{
+    class UserSettings : ApplicationSettingsBase
+    {
+        [UserScopedSetting()]
+        [DefaultSettingValue("Data Source=METRO-GP1;Integrated Security=SSPI;Initial Catalog=METRO")]
+        public string DatabaseConnectionString
+        {
+            get
+            {
+                return ((string)this["DatabaseConnectionString"]);
+            }
+            set
+            {
+                this["DatabaseConnectionString"] = (string)value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("")]
+        public string EpsonPriceListFilePath
+        {
+            get;
+            set;
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("")]
+        public string EnvisionCustomersFilePath
+        {
+            get;
+            set;
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("")]
+        public string SpaListFilePath
+        {
+            get;
+            set;
+        }
+    }
+}
